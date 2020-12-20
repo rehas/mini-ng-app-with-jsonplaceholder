1b583289483f43479139bfba1de097d5484d7692
rehas <beratson@gmail.com>
2020-12-20 17:16:15 +0100

    styles update, tasklist update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  singlePost = {
-    id: 5,
-    userId: 10
-  };
-  name = "Angular " + VERSION.major + " hellox!";
+  name = "Version: " + VERSION.major;
 }
